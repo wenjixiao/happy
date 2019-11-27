1120b4d674b68cae2f21e3dbfb6398005a5ef86c
wenjixiao <m15101577296@163.com>
2019-11-28 04:55:48 +0800

    can run v1

--- msgclient1.py
+++ msgclient1.py
@@ -6,12 +6,13 @@ import message_pb2 as message
 
 class MsgClientProtocol(msgprotocol.MsgProtocol):
     def __init__(self,on_con_lost,ui_obj):
+        msgprotocol.MsgProtocol.__init__(self)
         self.on_con_lost = on_con_lost
         self.ui_obj = ui_obj
     # override
     def connection_made(self,transport):
         msgprotocol.MsgProtocol.connection_made(self,transport)
-        wx.CallAfter(self.ui_obj.connection_made())
+        wx.CallAfter(self.ui_obj.connection_made)
     # override
     def connection_lost(self,exc):
         msgprotocol.MsgProtocol.connection_lost(self,exc)
@@ -30,31 +31,43 @@ class AsyncThread(threading.Thread):
         self.ui_obj = ui_obj
     
     async def inner_send_msg(self,msg):
+        print("inner_send_msg runned")
         self.transport.write(msgprotocol.packMsg(msg.SerializeToString()))
         
     def send_msg(self,msg):
-        self.loop.call_soon_threadsafe(self.inner_send_msg,msg)
+        print("in send_msg")
+        coro = self.inner_send_msg(msg)
+        asyncio.run_coroutine_threadsafe(coro,self.loop)
         
-    def run(self):
-        self.loop = asyncio.get_running_loop()
-        on_con_lost = loop.create_future()
-        self.transport,self.protocol = await loop.create_connection(
+    async def connect(self):
+        print("async thread begin ...")
+        on_con_lost = self.loop.create_future()
+        self.transport,self.protocol = await self.loop.create_connection(
             lambda: MsgClientProtocol(on_con_lost,self.ui_obj),'127.0.0.1',5678)
         try:
             await on_con_lost
         finally:
             transport.close()
+        print("async run ended")
+            
+    def run(self):
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+        self.loop.create_task(self.connect())
+        self.loop.run_forever()
 
 # Bind(event, handler, source=None, id=wx.ID_ANY, id2=wx.ID_ANY)
 class WeiqiClient(wx.Frame):
     def __init__(self, parent, title):
         super(WeiqiClient, self).__init__(parent, title=title, size=(600, 400))
         self.InitUI()
+        self.async_init()
         self.Centre()
         self.Show()
         
     def async_init(self):
         self.async_thread = AsyncThread(self)
+        self.async_thread.start()
         
     def InitUI(self):
         panel = wx.Panel(self)
@@ -79,20 +92,46 @@ class WeiqiClient(wx.Frame):
         panel.SetSizer(vbox)
 
     def on_run_button(self,event):
-        myinput = self.input_text.GetValue()
+        myline = self.input_text.GetValue().split()
+        cmd = myline[0]
         
-        # here tell async loop to send msg 
-        self.async_thread.send_msg(msg)
-        
-        self.input_text.Clear()
-        self.output_text.AppendText("\n")
-        self.output_text.AppendText(myinput)
+        if cmd == "logout":
+            msg = message.Msg()
+            msg.type = message.MsgType.LOGOUT
+            self.async_thread.send_msg(msg)
+            transport.close()
+            
+        elif cmd == "login":
+            msg = message.Msg()
+            msg.type = message.MsgType.LOGIN
+            msg.login.pid = myline[1]
+            msg.login.passwd = myline[2]
+            
+            self.async_thread.send_msg(msg)
+            # transport.write(msgprotocol.packMsg(msg.SerializeToString()))
+            
+        elif cmd == "info":
+            # print(protocol.transport)
+            msg = message.Msg()
+            msg.type = message.MsgType.INFO
+            # print("transport is closed? ",transport.is_closing())
+            self.async_thread.send_msg(msg)
+            # transport.write(msgprotocol.packMsg(msg.SerializeToString()))
+            
+        else:
+            print("***no that command!***\n")
         
     def receive_msg(self,msg):
-        pass
+        print("received msg: %s" % msg)
+        self.input_text.Clear()
+        self.output_text.AppendText("\n")
+        self.output_text.AppendText(str(msg))
     
+    def connection_made(self):
+        print("connection made")
+        
     def connection_lost(self,exc):
-        pass
+        print("connection lost @ %s" % exc)
     
     def OnCloseWindow(self,event):
         self.Destroy()
