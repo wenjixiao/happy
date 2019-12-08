2860c7dd34854d0b6d64f211ef2b5f8be51ca004
wenjixiao <m15101577296@163.com>
2019-12-08 08:25:26 +0800

    no no no

--- stupid/game.py
+++ stupid/game.py
@@ -2,56 +2,102 @@ import wx
 import pb.msg_pb2 as pb
 import logging
 
-class PlayGame(wx.Frame):
-	def __init__(self,parent,game):
-		super(PlayGame, self).__init__(parent, size=(400, 300))
+def otherColor(color):
+	return pb.Color.BLACK if color == pb.Color.WHITE else pb.Color.WHITE
+
+class PlayerPane(wx.Panel):
+	def __init__(self,parent,game,player,clock):
+		super(PlayerPane,self).__init__(parent)
 		self.game = game
-		self.SetTitle("***"+self.GetParent().player.pid+"/"+str(self.game.gid)+"***")
-
-		self.timer = wx.Timer(self)
-		self.Bind(wx.EVT_TIMER, self.OnTimer, self.timer)
-
-		panel = wx.Panel(self)
-
-		self.clock_text = wx.StaticText(panel,label="***my clock data***")
-		
-		self.output_text = wx.TextCtrl(panel, style = wx.TE_MULTILINE | wx.HSCROLL)
-		self.input_text = wx.TextCtrl(panel,style=wx.TE_PROCESS_ENTER)
-		self.input_text.Bind(wx.EVT_TEXT_ENTER,self.OnMyAction)
-
-		hbox = wx.BoxSizer()
-		hbox.Add(self.clock_text,flag=wx.ALIGN_CENTER)
+		self.player = player
+		self.pidSt = wx.StaticText(self,label = self.getPidStr())
+		self.levelSt = wx.StaticText(self,label = self.getLevelStr())
+		self.clockPane = ClockPane(self,game,clock)
 
 		vbox = wx.BoxSizer(wx.VERTICAL)
-		vbox.Add(hbox,proportion=0,flag=wx.ALIGN_CENTER)
-		vbox.Add(self.output_text,proportion=1, flag=wx.EXPAND | wx.ALL,border=4)
-		vbox.Add(self.input_text,flag=wx.EXPAND)
+		vbox.Add(self.pidSt,flag = wx.ALIGN_CENTER)
+		vbox.Add(self.levelSt,flag = wx.ALIGN_CENTER)
+		vbox.Add(self.clockPane,flag = wx.EXPAND)
 
-		panel.SetSizer(vbox)
+		self.SetSizer(vbox)
 
+	def getPidStr(self):
+		return str(self.player.pid)
+
+	def getLevelStr(self):
+		return str(self.player.level)
+
+class ClockPane(wx.Panel):
+	def __init__(self,parent,game,clock):
+		super(ClockPane,self).__init__(parent)
+		self.game = game
+		self.clock = clock
+		self.timer = wx.Timer(self)
+		self.Bind(wx.EVT_TIMER, self.onClick, self.timer)
+
+		self.baoLiuSt = wx.StaticText(self,label = self.getPaoLiuStr())
+		self.ciShuMeiCiSt = wx.StaticText(self,label = self.getCiShuMeiCiStr())
+		self.duMiaoSt = wx.StaticText(self,label = self.getDuMiaoStr())
+
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		vbox.Add(self.baoLiuSt,flag = wx.ALIGN_CENTER)
+		vbox.Add(self.ciShuMeiCiSt,flag = wx.ALIGN_CENTER)
+		vbox.Add(self.duMiaoSt,flag = wx.ALIGN_CENTER)
+		self.SetSizer(vbox)
+
+	def getPaoLiuStr(self):
+		return str(self.clock.baoLiu)
+
+	def getCiShuMeiCiStr(self):
+		return str(self.clock.ciShu)+"*"+str(self.game.proto.meiCi)
+
+	def getDuMiaoStr(self):
+		return str(self.clock.duMiao)
+
+	def resetDuMiao(self):
+		self.clock.duMiao = self.game.proto.duMiao
+
+	def resetMeiCi(self):
+		self.clock.duMiao = self.game.proto.meiCi
+
+	def updateView(self):
+		self.baoLiuSt.SetLabel(self.getPaoLiuStr())
+		self.ciShuMeiCiSt.SetLabel(self.getCiShuMeiCiStr())
+		self.duMiaoSt.SetLabel(self.getDuMiaoStr())
+
+	def onClick(self,event):
+		if self.clock.baoLiu != 0:
+			self.clock.baoLiu -= 1
+		elif self.clock.duMiao != 0:
+			self.clock.duMiao -= 1
+		elif self.clock.ciShu != 0:
+			self.clock.ciShu -= 1
+			# parent is game
+			self.resetMeiCi()
+		else:
+			# timeout
+			self.iamTimeout()
 		self.updateView()
-		self.Show()
-		self.checkStart()
 
-	def canPutStone(self):
-		return self.game.state == pb.State.READY or self.game.state == pb.State.RUNNING
-
-	def checkStart(self):
-		if self.canPutStone() and self.isMyTurn():
-			self.startMyClock()
-
-	def startMyClock(self):
+	def start(self):
 		self.timer.Start(1000)
 
-	def stopMyClock(self):
-		self.myClock().duMiao = self.game.proto.duMiao
+	def stop(self):
 		self.timer.Stop()
+		self.resetDuMiao()
+
+class BoardPane(wx.Panel):
+	def __init__(self,parent,game):
+		super(BoardPane,self).__init__(parent)
+		self.game = game
+		self.output_text = wx.TextCtrl(self, style = wx.TE_MULTILINE | wx.HSCROLL)
 
 	def updateView(self):
 		self.output_text.SetValue(str(self.game.stones))
 
-	def getOtherColor(self,color):
-		return pb.Color.BLACK if color == pb.Color.WHITE else pb.Color.WHITE
+	def addStone(self,stone):
+		self.game.stones.append(stone)
+		self.updateView()
 
 	def getNextColor(self):
 		c = len(self.game.stones) 
@@ -59,19 +105,59 @@ class PlayGame(wx.Frame):
 			return pb.Color.BLACK
 		else:
 			lastStone = self.game.stones[c-1]
-			return self.getOtherColor(lastStone.color)
+			return otherColor(lastStone.color)
 
-	def getMyColor(self):
+class PlayGame(wx.Frame):
+	def __init__(self,parent,game):
+		super(PlayGame, self).__init__(parent, size=(400, 300))
+		self.game = game
+		self.SetTitle("***"+self.GetParent().player.pid+"/"+str(self.game.gid)+"***")
+
+		panel = wx.Panel(self)
+
+		self.input_text = wx.TextCtrl(panel,style=wx.TE_PROCESS_ENTER)
+		self.input_text.Bind(wx.EVT_TEXT_ENTER,self.onMyAction)
+
+		hbox = wx.BoxSizer()
+		self.playerPanes = [PlayerPane(panel,self.game,p,c) for p,c in zip(self.game.players,self.game.clocks)]
+		for playerPane in self.playerPanes:
+			hbox.Add(playerPane)
+
+		vbox = wx.BoxSizer(wx.VERTICAL)
+		vbox.Add(hbox,proportion=0,flag=wx.ALIGN_CENTER)
+		self.boardPane = BoardPane(panel,self.game)
+		vbox.Add(self.boardPane,proportion=1, flag=wx.EXPAND | wx.ALL,border=4)
+		vbox.Add(self.input_text,flag=wx.EXPAND)
+
+		panel.SetSizer(vbox)
+
+		self.Show()
+		self.checkStart()
+
+	def canPutStone(self):
+		return self.game.state == pb.State.READY or self.game.state == pb.State.RUNNING
+
+	def startMyClock(self):
+		for playerPane in self.playerPanes:
+			if playerPane.player == self.myPlayer():
+				playerPane.clockPane.start()
+
+	def stopMyClock(self):
+		for playerPane in self.playerPanes:
+			if playerPane.player == self.myPlayer():
+				playerPane.clockPane.stop()
+
+	def checkStart(self):
+		if self.canPutStone() and self.isMyTurn():
+			self.startMyClock()
+
+	def myColor(self):
 		return pb.Color.BLACK if self.game.players[self.game.blackIndex] == self.myPlayer() else pb.Color.WHITE
-
-	def addStone(self,stone):
-		self.game.stones.append(stone)
-		self.updateView()
 
 	def putStone(self,stone):
 		if self.isMyTurn():
+			self.boardPane.addStone(stone)
 			self.stopMyClock()
-			self.addStone(stone)
 
 			msg = pb.Msg()
 			msg.type = pb.MsgType.HAND
@@ -79,31 +165,22 @@ class PlayGame(wx.Frame):
 			msg.hand.stone.CopyFrom(stone)
 			self.send_msg(msg)
 		else:
-			self.addStone(stone)
+			self.boardPane.addStone(stone)
 			self.checkStart()
 
 	def myPlayer(self):
 		return self.GetParent().player
 
-	def myIndex(self):
-		players = self.game.players
-		for i,p in enumerate(players):
-			if self.myPlayer() == p:
-				return i
-
-	def myClock(self):
-		return self.game.clocks[self.myIndex()]
-
 	def isMyTurn(self):
-		return self.getNextColor() == self.getMyColor()
+		return self.boardPane.getNextColor() == self.myColor()
 
-	def OnMyAction(self,event):
+	def onMyAction(self,event):
 		myline = self.input_text.GetValue().split()
 		cmd = myline[0]
 		if cmd == "stone":
 			if self.canPutStone() and self.isMyTurn():
 				stone = pb.Stone()
-				stone.color = self.getMyColor()
+				stone.color = self.myColor()
 				stone.x = int(myline[1])
 				stone.y = int(myline[2])
 				self.putStone(stone)
@@ -113,31 +190,10 @@ class PlayGame(wx.Frame):
 
 	def iamTimeout(self):
 		logging.info("timeout:")
-		self.stopMyClock()
 
 		self.game.state = pb.State.ENDED
-		self.game.result.winner = self.getOtherColor(self.getMyColor())
+		self.game.result.winner = otherColor(self.myColor())
 		self.game.result.endType = pb.EndType.TIMEOUT
 
 		# tell server i timeout and gameover
 		pass
-
-	def updateClock(self):
-		clock = self.myClock()
-		s = str(clock.baoLiu)+"--"+str(clock.ciShu)+"--"+str(clock.duMiao)
-		self.clock_text.SetLabel(s)
-
-	def OnTimer(self, evt):
-		clock = self.myClock()
-		if clock.baoLiu != 0:
-			clock.baoLiu -= 1
-		elif duMiao != 0:
-			clock.duMiao -= 1
-		elif ciShu != 0:
-			clock.ciShu -= 1
-			clock.duMiao = self.game.proto.meiCi
-		else:
-			# timeout
-			self.iamTimeout()
-
-		self.updateClock()
