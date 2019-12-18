d6643dd8f9e2bb4e5e43b3d62d765642ebcea0a5
wenjixiao <m15101577296@163.com>
2019-12-18 21:02:34 +0800

    cat love fish

--- stupid/board.py
+++ stupid/board.py
@@ -1,63 +1,78 @@
 import wx
 import pb.msg_pb2 as pb
 # import logging
-from common import otherColor
+from common import *
 from itertools import product
 
 class BoardPane(wx.Panel):
 	def __init__(self,parent,gameFrame):
 		super(BoardPane,self).__init__(parent)
-		self.SetBackgroundColour("#FFC0CB")
+		# self.SetBackgroundColour("#FFC0CB")
 		self.gameFrame = gameFrame
+
 		self.Bind(wx.EVT_PAINT,self.OnPaint)
 		self.Bind(wx.EVT_SIZE,self.OnSize)
 		self.Bind(wx.EVT_LEFT_DOWN, self.OnLeftDown)
-		self.stones = []
 
 	def getGame(self):
 		return self.gameFrame.game
 
 	def getStones(self):
 		return self.getGame().stones
-
-	def OnLeftDown(self,e):
-		p = e.GetPosition()
-		print(self.dev2user(p))
-		# x, y = self.ClientToScreen(e.GetPosition())
-		# ox, oy = self.GetPosition()
-
-	def user2dev(self,userPoint):
-		"devX = userX+0.5*width,devY = 0.5*height-userY"
+# ---------------------------------------------------------
+	def getSize(self):
 		width,height = self.GetClientSize()
 		halfWidth,halfHeight = int(width/2),int(height/2)
 		radius = int(min(width,height)/(20*2))
 		unit = radius * 2
+		return (halfWidth,halfHeight,radius)
 
+	def user2dev(self,userPoint):
+		''' 
+			devX = userX+0.5*width
+			devY = 0.5*height-userY
+		'''
+		halfWidth,halfHeight,radius = self.getSize()
+		unit = radius * 2
+
+		# 这里和公式不同的是，乘以了一个unit！
+		# 以一个格子的长度为一个缩放单位，协调用户坐标和设备坐标。
 		devX = userPoint.x*unit + halfWidth
 		devY = halfHeight - userPoint.y*unit
 		return wx.Point(devX,devY)
 
 	def dev2user(self,devPoint):
-		"userX = devX - halfWidth,userY = halfHeight - devY"
-		width,height = self.GetClientSize()
-		halfWidth,halfHeight = int(width/2),int(height/2)
-		radius = int(min(width,height)/(20*2))
+		'''
+			userX = devX - halfWidth
+			userY = halfHeight - devY
+		'''
+		halfWidth,halfHeight,radius = self.getSize()
 		unit = radius * 2
 
+		# 这里得到的是一个浮点数，也许-1.75，也许5.32，等等。
+		# 找到和浮点数相近的整数，即用户坐标。
 		shangX = (devPoint.x - halfWidth) / unit
 		shangY = (halfHeight - devPoint.y) / unit
 
 		return wx.Point(round(shangX),round(shangY))
-
+# ---------------------------------------------------------
 	def OnSize(self,event):
 		self.Refresh()
 
+	def OnLeftDown(self,e):
+		userPoint = self.dev2user(e.GetPosition())
+		if validate(userPoint):
+			if self.gameFrame.myClockPane().isRunning():
+				stone = pb.Stone()
+				stone.color = self.gameFrame.myColor()
+				stone.x = userPoint.x
+				stone.y = userPoint.y
+				self.gameFrame.putStone(stone)
+				
 	def OnPaint(self,event):
 		dc = wx.PaintDC(self)
 
-		width,height = self.GetClientSize()
-		halfWidth,halfHeight = int(width/2),int(height/2)
-		radius = int(min(width,height)/(20*2))
+		_,_,radius = self.getSize()
 
 		# draw 19*19 lines
 		for i in range(-9,10):
@@ -66,17 +81,17 @@ class BoardPane(wx.Panel):
 
 		# draw 9 stars
 		for (x,y) in product([-6,0,6],repeat=2):
-			dc.DrawCircle(self.user2dev(wx.Point(x,y)),int(radius/3))
+			dc.SetBrush(wx.Brush("#FF7F00"))
+			dc.DrawCircle(self.user2dev(wx.Point(x,y)),3)
 
 		# draw stones
-		for stone in self.stones:
+		for stone in self.getStones():
 			color = wx.BLACK if stone.color == pb.Color.BLACK else wx.WHITE
 			dc.SetBrush(wx.Brush(color))
 			dc.DrawCircle(self.user2dev(wx.Point(stone.x,stone.y)),radius)
 
 	def addStone(self,stone):
-		# self.getStones().append(stone)
-		self.stones.append(stone)
+		self.getStones().append(stone)
 		self.Refresh()
 
 	def addStones(self,stones):
--- stupid/common.py
+++ stupid/common.py
@@ -1,5 +1,8 @@
 import pb.msg_pb2 as pb
 import logging
 
+def validate(point):
+	return point.x >= -9 and point.x <= 9 and point.y >= -9 and point.y <= 9
+
 def otherColor(color):
 	return pb.Color.BLACK if color == pb.Color.WHITE else pb.Color.WHITE
