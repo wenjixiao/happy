aab937d3ec50fa698c00eb9303d79a02f37d942d
wenjixiao <m15101577296@163.com>
2020-01-09 13:14:50 +0800

    寻寻觅觅，冷冷清清，凄凄惨惨戚戚

---
+++ moon/src/client.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"mynet"
+	"net"
+	"os"
+	"pb"
+	"strings"
+)
+
+var done = make(chan struct{})
+
+type ClientProtocol struct {
+	mynet.MsgProtocol
+	Conn   net.Conn
+}
+
+func (protocol *ClientProtocol) ConnectionMade(conn net.Conn) {
+	protocol.Conn = conn
+}
+
+func (protocol *ClientProtocol) ProcessMsg(msgBytes []byte) {
+	msg := DecodeMsg(msgBytes)
+	log.Printf("protocol received: %v", msg)
+	switch msg.GetType() {
+	case pb.Type_LOGIN_RESULT:
+		loginResult := msg.GetLoginResult()
+		log.Println(loginResult)
+	}
+}
+
+func (protocol *ClientProtocol) ConnectionLost(err error) {
+	log.Printf("connection lost: %v", err)
+	done <- struct{}{}
+}
+
+func CmdLoop(protocol *ClientProtocol) {
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if line == "" {
+			continue
+		}
+
+		cmdArray := strings.Split(line, " ")
+		cmd, params := cmdArray[0], cmdArray[1:]
+
+		if cmd == "login" {
+			pid, password := params[0], params[1]
+			msg := &pb.Msg{Type: pb.Type_LOGIN, Union: &pb.Msg_Login{&pb.Login{Pid: pid, Password: password}}}
+			SendMsg(protocol.Conn, msg)
+		} else if cmd == "exit" {
+			//我关闭conn的时候，reading goroutine自然会退出，因为它在一直读那个conn
+			protocol.Conn.Close()
+			break
+		} else {
+			log.Println("*No* that cmd!")
+		}
+	}
+	log.Println("cmd loop exited!")
+}
+
+func main() {
+	addr := "localhost:20000"
+	conn, err := net.Dial("tcp", addr)
+	defer conn.Close()
+	if err != nil {
+		log.Fatalf("connect error: %v", err)
+	}
+
+	protocol := &ClientProtocol{}
+	protocol.Conn = conn
+	protocol.Receiver = protocol
+
+	go mynet.HandleConn(conn, protocol)
+
+	CmdLoop(protocol)
+
+	<-done
+
+	log.Println("-----main exit----")
+}
--- moon/src/server.go
+++ moon/src/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/golang/protobuf/proto"
 	"log"
 	"mynet"
 	"net"
@@ -30,33 +29,6 @@ type Message struct {
 type OpProtocol struct {
 	AddOrRemove bool
 	Protocol    *ServerProtocol
-}
-
-//---------------------------------------------------------
-
-func EncodeMsg(msg *pb.Msg) []byte {
-	data, err := proto.Marshal(msg)
-	if err != nil {
-		log.Fatalf("protobuf marshal: %v\n", err)
-	}
-	return data
-}
-
-func DecodeMsg(msgBytes []byte) *pb.Msg {
-	msg := &pb.Msg{}
-	err := proto.Unmarshal(msgBytes, msg)
-	if err != nil {
-		log.Fatalf("protobuf unmarshal: %v", err)
-	}
-	return msg
-}
-
-func SendMsg(conn net.Conn, msg *pb.Msg) {
-	data := EncodeMsg(msg)
-	_, err := conn.Write(mynet.AddHeader(data))
-	if err != nil {
-		log.Fatalf("conn write: %v\n", err)
-	}
 }
 
 //---------------------------------------------------------
